959cfc1fb1d61d8687afb23afe33c072bf272502
Kuzya <alexkuz@gmail.com>
2017-03-05 11:57:50 +0300

    upgrade examples

--- examples/webpack.config.js
+++ examples/webpack.config.js
@@ -1,41 +1,50 @@
 var path = require('path');
 var webpack = require('webpack');
 
+var isProduction = process.env.NODE_ENV === 'production';
+
 module.exports = {
-  devtool: 'eval',
+  devtool: 'source-map',
   entry: [
-    'webpack-dev-server/client?http://localhost:3000',
-    'webpack/hot/only-dev-server',
+    !isProduction && 'webpack-dev-server/client?http://localhost:3000',
+    !isProduction && 'webpack/hot/only-dev-server',
     './src/index'
-  ],
+  ].filter(Boolean),
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
     publicPath: '/static/'
   },
   plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+      }
+    }),
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
-  ],
+    new webpack.NoEmitOnErrorsPlugin(),
+    isProduction && new webpack.optimize.UglifyJsPlugin({
+      compress: { warnings: false },
+      output: { comments: false },
+      sourceMap: true
+    })
+  ].filter(Boolean),
   resolve: {
     alias: {
       'react-json-tree/lib': path.join(__dirname, '..', 'src'),
       'react-json-tree': path.join(__dirname, '..', 'src'),
       'react': path.join(__dirname, 'node_modules', 'react')
     },
-    extensions: ['', '.js']
-  },
-  resolveLoader: {
-    'fallback': path.join(__dirname, 'node_modules')
+    extensions: ['.js']
   },
   module: {
     loaders: [{
       test: /\.js$/,
-      loaders: ['react-hot', 'babel'],
+      loaders: [!isProduction && 'react-hot-loader', 'babel-loader'].filter(Boolean),
       include: path.join(__dirname, 'src')
     }, {
       test: /\.js$/,
-      loaders: ['react-hot', 'babel'],
+      loaders: [!isProduction && 'react-hot-loader', 'babel-loader'].filter(Boolean),
       include: path.join(__dirname, '..', 'src')
     }]
   }
