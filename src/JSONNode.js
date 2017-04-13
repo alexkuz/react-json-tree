2050124cb4aa152ec109cf4a55c7f48b3e93b8cc
borm <efim4eg@icloud.com>
2017-04-13 14:42:14 +0300

    fix PropTypes

--- src/JSONNode.js
+++ src/JSONNode.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import objType from './objType';
 import JSONObjectNode from './JSONObjectNode';
 import JSONArrayNode from './JSONArrayNode';
